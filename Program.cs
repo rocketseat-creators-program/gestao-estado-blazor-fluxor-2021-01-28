8734e0829385c5e5e517dfe5f6daf7e6a5a9ef44
Alexandre Costa <alexandresantoscosta@gmail.com>
2022-01-27 22:07:06 -0300

    Configurado Fluxor

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Fluxor;
+using Fluxor.Blazor.Web;
 using GestaoEstadoFluxor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -7,5 +9,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddFluxor(f => f.ScanAssemblies(typeof(Program).Assembly));
 
 await builder.Build().RunAsync();
